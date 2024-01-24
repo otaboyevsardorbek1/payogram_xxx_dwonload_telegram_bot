dbe3fd890a3a75b76711ebab6561115ae0b92e3b
sardorbek otaboyev <127109664+otaboyevsardorbek1@users.noreply.github.com>
2024-01-24 15:09:06 +0500

    Update config.py
    ADD NEW ADMIN ID

--- PornHub/config.py
+++ PornHub/config.py
@@ -4,9 +4,9 @@ API_ID: int = insert_your_own_api_id_here
 API_HASH: str = "insert_your_own_api_hash_here"
 TOKEN: str = "insert_your_bot_token_here"
 
-log_chat: int = -1001662591986
-sub_chat: str = "SakuraEmpireTeam"
-sudoers: List[int] = [2137482758, 1249591948]
+log_chat: int = -1001904022511
+sub_chat: str = "algo_ubtuit_uz_23"
+sudoers: List[int] = [5765540159, 6104450910]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
 
 # notes
